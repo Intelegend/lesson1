ae8300020686f6005ad6315f5b3962566b8ed290
Intelegend <47541432+Intelegend@users.noreply.github.com>
2021-04-05 17:27:46 +0300

    Задание 2 CRUD-система исправление

--- local/modules/rest.monitoring/lib/Profile.php
+++ local/modules/rest.monitoring/lib/Profile.php
@@ -3,31 +3,58 @@
 
 namespace Rest\Monitoring;
 
-
+/**
+ * Изменение таблицы профилей.
+ *
+ * @property integer $id
+ * @property var $result
+ */
 class Profile
 {
-
+    /**
+     * Сортировка данных таблицы.
+     *
+     * @param select нужен для выборки определенных полей
+     * @param filter нужен для сортировки по заданому условию
+     * @param order позволяет указать порядок сортировки
+     * @param limit служит для получения определенного колличества записей
+     * @property var $result служит для вывода конечной информации
+     */
     public function getList()
     {
         $result=y_monitoring_profiles::GetList(array(),
-            array('ID' =>DESC,
-            ));
-        var_dump( $result);
+            array('ID' =>'DESC',
+                'select'  => array("NAME","URL","METHOD","CHECK_INTERVAL"),
+                'filter'  =>  array('=ID' => 1),
+                'order' => array('NAME' => 'DESC', 'METHOD' => 'ASC'),
+                'limit'   => 10 ));
+        var_dump($result);
     }
-
+    /**
+     * Получения индентификатора из таблицы.
+     *
+     * @param ID поле индетнтификатора
+     * @property integer $id переменная для записи индентификатора
+     */
     public function getById($id)
     {
-        $arSelect = Array("ID", "NAME","URL","METHOD","CHECK_INTERVAL","ACTIVITY","STATUS_RESULT");
-        $res = y_monitoring_profiles::GetList(Array(),$arSelect);
-        while($ob = $res->GetNextElement()) {
-            $arFields = $ob->GetFields();
-            $id = y_monitoring_profiles::GetByID($arFields['ID']);
-        }
-
+        $id = y_monitoring_profiles::getByPrimary(array('ID' => 1));
+        $arr=array("NAME","URL","METHOD","CHECK_INTERVAL","ACTIVITY", "STATUS_RESULT");
     }
-
+    /**
+     * Изменение данных в таблице.
+     *
+     * @param NAME Название
+     * @param URL URL сервиса
+     * @param METHOD Метод запроса
+     * @param CHECK_INTERVAL Интервал проверки(мин.)
+     * @param ACTIVITY Активность
+     * @param STATUS_RESULT Результат проверки
+     * @property integer $id переменная для записи индентификатора
+     */
     public function edit($id)
     {
+
         $result = y_monitoring_profiles::update($id, array(
         "NAME" => 'varchar',
         "URL" => 'varchar',
@@ -41,7 +68,17 @@ class Profile
             $errors = $result->getErrorMessages();
         }
     }
-
+    /**
+     * Изменение данных в таблице.
+     *
+     * @param NAME Название
+     * @param URL URL сервиса
+     * @param METHOD Метод запроса
+     * @param CHECK_INTERVAL Интервал проверки(мин.)
+     * @param ACTIVITY Активность
+     * @param STATUS_RESULT Результат проверки
+     * @property var $result служит для вывода конечной информации
+     */
     public function add()
     {
         $result = y_monitoring_profiles::add(array(
@@ -62,7 +99,11 @@ class Profile
             $errors = $result->getErrorMessages();
         }
     }
-
+    /**
+     * Изменение данных в таблице.
+     * @property integer $id переменная для записи индентификатора
+     * @property var $result служит для вывода конечной информации
+     */
     public function delete($id)
     {
         $result = y_monitoring_profiles::delete($id);
